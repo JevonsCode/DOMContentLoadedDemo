09aa173dcd939fe03143aacba1779d25bff6f0de
jevons <36203025+jevonsofcode@users.noreply.github.com>
2018-11-08 09:55:46 +0800

    Update print.js

--- print.js
+++ print.js
@@ -1,1 +1,5 @@
 console.log('print>>>>>>>>>>')
+document.addEventListener("DOMContentLoaded",function(){
+    let span=document.getElementById("demo");
+    console.log(span,"DOMContentLoaded静态服务器")
+})
